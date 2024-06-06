71dd73807af138a2641f3055297857274d1d040d
MykeLora <maycollora16@gmail.com>
2024-06-06 17:50:22 -0600

    Agregando los Dtos de la entidad categoria para posteriormente usarlos en la interface de la misma.

---
+++ Hotel.Application/Contract/ICategoryService.cs
@@ -0,0 +1,13 @@
+﻿using Hotel.Domain.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel.Application.Contract
+{
+    public interface ICategoryService : IBaseRepository<>
+    {
+    }
+}
---
+++ Hotel.Application/Dtos/Category/CategoryDtoRemove.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel.Application.Dtos.Category
+{
+    public class CategoryDtoRemove
+    {
+        public int IdCategoria { get; set; }
+    }
+}
