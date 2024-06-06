a8f536337ff5249d755328a6b5f6fe8bbc8c1f22
MykeLora <maycollora16@gmail.com>
2024-06-06 17:35:14 -0600

    Agregando los folder a la capa de Aplicacion,configurando el servicio base y el resultado del mismo.

---
+++ Hotel.Application/Core/IBaseService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel.Application.Core
+{
+    public interface IBaseService<TDtoAdd,  TDtoRemove, TDtoUpdate, TModel>
+    {
+        ServiceResut<List<TModel>> GetAll();
+        ServiceResut<TModel> GetById(int id);
+        ServiceResut<TModel> Save(TDtoAdd dtoAdd);
+        ServiceResut<TModel> Update(TDtoUpdate dtoUpdate);
+        ServiceResut<TModel> Remove(TDtoRemove dtoRemove);
+    }
+}
